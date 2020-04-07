2592b53c0a361949e2aa81dd1914c87c1db6655c
melody <269786606@qq.com>
2020-04-08 06:52:21 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Login msg="欢迎来到登录页面"/>
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: -8px -8px 0px -8px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login">
+        <div>123</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        props: {
+            msg: String
+        }
+    }
+</script>
+<style scoped>
+    .login{
+        background-color: #0a1982;
+    }
+</style>
